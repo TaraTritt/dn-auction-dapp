40dfa5a08ab7c205666cde61a91008e60eca14e3
MiwaTritt <tmt7rc@virginia.edu>
2018-03-01 22:49:13 -0500

    Adding next-routes and template for configure new auction page

---
+++ pages/auctions/new.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Card, Button, Icon, Grid } from "semantic-ui-react";
+
+import web3 from "../../ethereum/web3";
+import Layout from "../../components/Layout";
+
+class AuctionNew extends Component {
+  render() {
+    return (
+      <Layout>
+        <h3>Configure New Auction</h3>
+      </Layout>
+    );
+  }
+}
+
+export default AuctionNew;
---
+++ routes.js
@@ -0,0 +1,5 @@
+const routes = require("next-routes")();
+
+routes.add("/auctions/new", "/auctions/new");
+
+module.exports = routes;
