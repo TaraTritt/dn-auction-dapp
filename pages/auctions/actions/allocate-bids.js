f36e7d725ce5533c4c6cc59344386c476d06c6b8
MiwaTritt <tmt7rc@virginia.edu>
2018-03-04 14:36:36 -0500

    Initial form for allocate bids page

--- pages/auctions/actions/allocate-bids.js
+++ pages/auctions/actions/allocate-bids.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
-import { Form, Grid } from "semantic-ui-react";
+import { Form, Button, Message } from "semantic-ui-react";
 
 import web3 from "ethereum/web3";
 import Layout from "components/Layout";
 import AuctionActionHeader from "components/auctions/actions/AuctionActionHeader";
 
 class AuctionAllocateBids extends Component {
+  state = {
+    errorMessage: "",
+    successMessage: "",
+    loading: false
+  };
+
   static getInitialProps(props) {
     return { auctionAddress: props.query.address };
   }
+
+  allocateBids = async event => {
+    this.setState({ loading: true, errorMessage: "", successMessage: "" });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const auction = Auction(this.props.auctionAddress);
+
+      await auction.methods.allocateBids().send({ from: accounts[0] });
+
+      // reset state
+      this.setState({
+        successMessage:
+          "A new Discount Note has been created to the auction at address: " +
+          this.props.auctionAddress +
+          ". Go back to the Auction Details page to view it."
+      });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
 
   render() {
     return (
@@ -17,6 +46,26 @@ class AuctionAllocateBids extends Component {
           auctionAddress={this.props.auctionAddress}
           title="Allocate Bids"
         />
+        <Form
+          onSubmit={this.allocateBids}
+          error={!!this.state.errorMessage}
+          success={!!this.state.successMessage}
+        >
+          <Message error header="Error" content={this.state.errorMessage} />
+          <Message
+            success
+            header="Success"
+            content={this.state.successMessage}
+          />
+          <Button
+            type="submit"
+            loading={this.state.loading}
+            primary
+            style={{ marginTop: "13px" }}
+          >
+            Allocate Bids
+          </Button>
+        </Form>
       </Layout>
     );
   }
