d4ec9d510728b1553bbb1f9a374579030df5f136
MiwaTritt <tmt7rc@virginia.edu>
2018-03-04 14:47:12 -0500

    Initial form for place bid page

--- pages/auctions/actions/place-bid.js
+++ pages/auctions/actions/place-bid.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Button, Message, Input } from "semantic-ui-react";
 
 import web3 from "ethereum/web3";
 import Layout from "components/Layout";
 import AuctionActionHeader from "components/auctions/actions/AuctionActionHeader";
 
 class AuctionPlaceBid extends Component {
+  state = {
+    maturityAmount: 0,
+    errorMessage: "",
+    successMessage: "",
+    loading: false
+  };
+
   static getInitialProps(props) {
     return { auctionAddress: props.query.address };
   }
+
+  placeBid = async event => {
+    this.setState({ loading: true, errorMessage: "", successMessage: "" });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const auction = Auction(this.props.auctionAddress);
+
+      await auction.methods
+        .placeBid(this.state.maturityAmount, this.props.noteDetails.address)
+        .send({ from: accounts[0] });
+
+      // reset state
+      this.setState({
+        successMessage:
+          "A bid has been placed for the auction at address: " +
+          this.props.auctionAddress +
+          " for the Discount Note at address " +
+          this.props.noteDetails.address
+      });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
 
   render() {
     return (
@@ -17,6 +50,37 @@ class AuctionPlaceBid extends Component {
           auctionAddress={this.props.auctionAddress}
           title="Place Bid"
         />
+        <Form
+          onSubmit={this.allocateBids}
+          error={!!this.state.errorMessage}
+          success={!!this.state.successMessage}
+        >
+          <Form.Field required>
+            <Input
+              label="Maturity Amount"
+              type="number"
+              icon="money"
+              value={this.state.maturityAmount}
+              onChange={event =>
+                this.setState({ maturityAmount: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Message error header="Error" content={this.state.errorMessage} />
+          <Message
+            success
+            header="Success"
+            content={this.state.successMessage}
+          />
+          <Button
+            type="submit"
+            loading={this.state.loading}
+            primary
+            style={{ marginTop: "13px" }}
+          >
+            Allocate Bids
+          </Button>
+        </Form>
       </Layout>
     );
   }
